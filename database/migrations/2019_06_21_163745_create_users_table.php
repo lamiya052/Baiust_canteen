1c1291efc2cd02c55240c3e43a0926645859d271
Lamiya Zahan <lamiyazahan052@gmail.com>
2019-06-25 01:22:58 +0600

    add all files

---
+++ database/migrations/2019_06_21_163745_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('email');
+            $table->string('roll_no');
+            $table->string('password');
+            $table->integer('department_id');
+            $table->integer('role_type_id');
+            $table->integer('user_type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
